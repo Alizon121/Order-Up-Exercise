1079a9f956138728f89ed2917e30ede7704eac93
Andrew <andrew.lizon12@gmail.com>
2025-01-10 12:17:05 -0800

    Added SQLAlchemy to app

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from app.config import Configuration
 from app.routes import orders
+from .models import db
 
 app = Flask(__name__) # declare the app
 app.config.from_object(Configuration) # Use the configuration class for config
 app.register_blueprint(orders.bp) # register the blueprint defined in orders.py
+db.init_app(app) # Configure the app with SQLAlchemy
+
+# init_app() method is used to configure the app for larger apps
+
